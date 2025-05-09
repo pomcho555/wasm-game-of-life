3e72a068e0df03bc0e2dbfb642edce497d1b16b6
pomcho555 <pomcho555@users.noreply.github.com>
2025-05-09 16:31:34 +0900

    Restore repository to state of commit v4

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,22 @@
 mod utils;
 
-use fixedbitset::FixedBitSet;
 use wasm_bindgen::prelude::*;
 
 extern crate js_sys;
-extern crate fixedbitset;
 
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
 
 #[wasm_bindgen]
 pub struct Universe {
     width: u32,
     height: u32,
-    cells: FixedBitSet,
+    cells: Vec<Cell>,
 }
 
 /// Public methods, exported to JavaScript.
@@ -47,13 +52,15 @@ impl Universe {
                 let cell = self.cells[idx];
                 let live_neighbors = self.live_neighbor_count(row, col);
 
-                next.set(idx, match (cell, live_neighbors) {
-                    (true, x) if x < 2 => false,
-                    (true, 2) | (true, 3) => true,
-                    (true, x) if x > 3 => false,
-                    (false, 3) => true,
-                    (otherwise, _) => otherwise
-                });
+                let next_cell = match (cell, live_neighbors) {
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    (Cell::Dead, 3) => Cell::Alive,
+                    (otherwise, _) => otherwise,
+                };
+
+                next[idx] = next_cell
             }
         }
 
@@ -63,26 +70,27 @@ impl Universe {
     pub fn new() -> Universe {
         let width = 64;
         let height = 64;
-    
-        let size = (width * height) as usize;
-        let mut cells = FixedBitSet::with_capacity(size);
 
-        for i in 0..size {
-            if js_sys::Math::random() < 0.5 {
-                cells.insert(i);
-            }
-        }
-    
-        // fixed initialized life
-        // for i in 0..size {
-        //     cells.set(i, i % 2 == 0 || i % 7 == 0);
-        // }
-    
+        // rand crate probably doesn't work on WASM so let's use js random
+        let cells = (0..width * height)
+            .map(|_| {
+                if js_sys::Math::random() < 0.5 {
+                    Cell::Alive
+                } else {
+                    Cell::Dead
+                }
+            })
+            .collect();
+
         Universe {
             width,
             height,
             cells,
         }
+    }
+
+    pub fn render(&self) -> String {
+        self.to_string()
     }
 
     pub fn width(&self) -> u32 {
@@ -92,8 +100,24 @@ impl Universe {
     pub fn height(&self) -> u32 {
         self.height
     }
-    
-    pub fn cells(&self) -> *const u32 {
-        self.cells.as_slice().as_ptr() as *const u32
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
+}
+
+use std::fmt;
+
+impl fmt::Display for Universe {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for line in self.cells.as_slice().chunks(self.width as usize) {
+            for &cell in line {
+                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
+                write!(f, "{}", symbol)?;
+            }
+            write!(f, "\n")?;
+        }
+
+        Ok(())
     }
 }
