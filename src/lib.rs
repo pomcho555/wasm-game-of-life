45c12f6c46247e22490b0490a4270145a3378949
pomcho555 <pomcho555@users.noreply.github.com>
2025-05-09 17:24:54 +0900

    Add lib tests

--- src/lib.rs
+++ src/lib.rs
@@ -104,6 +104,36 @@ impl Universe {
     pub fn cells(&self) -> *const Cell {
         self.cells.as_ptr()
     }
+
+    /// Set the width of the universe.
+    ///
+    /// Resets all cells to the dead state.
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
+    }
+
+    /// Set the height of the universe.
+    ///
+    /// Resets all cells to the dead state.
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
+    }
+}
+
+impl Universe {
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    // hint: use reference instead of new list of tuple to steal ownership and to save memory.
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, column) in cells.iter().cloned() {
+            let idx = self.get_index(row, column);
+            self.cells[idx] = Cell::Alive;
+        }
+    }
 }
 
 use std::fmt;
